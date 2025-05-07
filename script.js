7af88bcd53682c98128cfb1cd0ed5f9f625b276f
Warren Brown <warrengbrn@gmail.com>
2025-05-07 14:05:51 -0400

    updaated buttons for thin displays

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-require([ "esri/Map", "esri/views/MapView" ], (Map, MapView) => {
+require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
 
-    const map = new Map({
-      basemap: "topo-vector"
-    });
-  
-    const view = new MapView({
-      container: "viewDiv",
-      map: map,
-      zoom: 4,
-      center: [290, 25]
-    });
-  
-
-    view.ui.remove("zoom");
+  const map = new Map({
+    basemap: "topo-vector"
   });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    zoom: 4,
+    center: [290, 25]
+  });
+
+  view.ui.remove("zoom");
+});
+
+const switchMapChart = () => {
+  
+  const div = document.getElementById("switchIcon");
+
+  if (div.classList.contains("fa-globe")) {
+    div.classList.replace("fa-globe", "fa-chart-simple")
+  } else {
+    div.classList.replace("fa-chart-simple", "fa-globe")
+  }
+}
