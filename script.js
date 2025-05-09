8ca6e1a54af3977dab11388f41ea920d70a165d6
Warren Brown <warrengbrn@gmail.com>
2025-05-09 12:14:47 -0400

    edited startup script & added responsive grid to header buttons

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/widgets/Legend"], (Map, MapView, FeatureLayer, Legend) => {
 
-  
   const layer = new FeatureLayer({
     // portalItem: {
     //   url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/Global_Hex_Grid_50km/FeatureServer/0"
     // }
 
-    url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/Global_Hex_Grid_50km/FeatureServer/0"    
+    // url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/Global_Hex_Grid_50km/FeatureServer/0"    
   });
 
   const map = new Map({
-    basemap: "dark-gray",
+    basemap: "topo-vector",
     layers: [layer]
   });
 
